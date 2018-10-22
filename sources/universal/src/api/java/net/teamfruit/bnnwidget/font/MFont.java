c3466670dde69e7fb1fbe96cb80b73e013ca9a24
Kamesuta <kamesuta@gmail.com>
2018-10-23 02:41:02 +0900

    mfont

---
+++ sources/universal/src/api/java/net/teamfruit/bnnwidget/font/MFont.java
@@ -0,0 +1,62 @@
+package net.teamfruit.bnnwidget.font;
+
+import javax.annotation.Nonnull;
+
+import net.minecraft.client.gui.FontRenderer;
+import net.teamfruit.bnnwidget.compat.Compat.CompatFontRenderer;
+import net.teamfruit.bnnwidget.compat.OpenGL;
+
+public class MFont implements WFont {
+	private final @Nonnull CompatFontRenderer font;
+
+	public MFont(final @Nonnull FontRenderer font) {
+		this.font = new CompatFontRenderer(font);
+	}
+
+	@Override
+	public void drawString(final FontPosition p) {
+		final String text = p.getText();
+		final float scale = p.getScale();
+		final float width = this.font.getStringWidth(text)*scale;
+		final float height = this.font.getFontRendererObj().FONT_HEIGHT*scale;
+		float x;
+		switch (p.getAlign()) {
+			default:
+			case LEFT:
+				x = p.getX();
+				break;
+			case CENTER:
+				x = p.getX()-width/2;
+				break;
+			case RIGHT:
+				x = p.getX()-width;
+				break;
+		}
+		float y;
+		switch (p.getVAlign()) {
+			default:
+			case TOP:
+				y = p.getY();
+				break;
+			case MIDDLE:
+				y = p.getY()-height/2;
+				break;
+			case BOTTOM:
+				y = p.getY()-height;
+				break;
+		}
+		final int color = OpenGL.glGetColorRGBA();
+		OpenGL.glColorRGBA(0xffffffff);
+		OpenGL.glPushMatrix();
+		OpenGL.glScalef(scale, scale, 1);
+		this.font.drawString(p.getText(), x, y, color, false);
+		OpenGL.glPopMatrix();
+		OpenGL.glColorRGBA(color);
+	}
+
+	@Override
+	public float getWidth(final FontPosition p) {
+		final float scale = p.getScale();
+		return this.font.getStringWidthWithoutFormattingCodes(p.getText())*scale;
+	}
+}
